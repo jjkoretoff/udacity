2f33d2e2812e0bed047c524c8ea33d13817db555
jjkoretoff <jjkoretoff@gmail.com>
2016-04-19 20:47:28 -0400

    completed project

--- programming-foundations-with-python/break.py
+++ programming-foundations-with-python/break.py
@@ -1,3 +1,11 @@
+import time
 import webbrowser
 
-webbrowser.open("https://www.youtube.com/watch?v=gq2ZJ418ad8")
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while(break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=gq2ZJ418ad8")
+    break_count = break_count + 1
