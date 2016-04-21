576366e6c5e30d9476cac78a121f3b56d8c6b45b
jjkoretoff <jjkoretoff@gmail.com>
2016-04-21 17:11:19 -0400

    updates

--- programming-foundations-with-python/turtle-mini-project.py
+++ programming-foundations-with-python/turtle-mini-project.py
@@ -1,12 +1,11 @@
 #build a circle by rotating a square
-#same code to make circle of traingles but change .right to 120
 
 import turtle
 
-def draw_square(some_turtle):
-    for i in range(1, 5):
+def draw_triangle(some_turtle):
+    for i in range(1, 4):
         some_turtle.forward(100)
-        some_turtle.right(90)
+        some_turtle.right(120)
 
 def draw_art():
     window = turtle.Screen()
@@ -17,12 +16,15 @@ def draw_art():
     brad.color("yellow")
     brad.speed("fast")
     #want to create loop to repeat
-    for i in range(1, 36):
+    for i in range(1, 18):
     #call the draw_square function/loop we wrote above for brad
-        draw_square(brad)
+        draw_triangle(brad)
     #make the turtle turn by a certain number of degrees
-        brad.right(10)
+        brad.right(20)
+    brad.circle(50)
+    brad.home()
+    brad.seth(270)
+    brad.forward(200)
     window.exitonclick
 
 draw_art()
-    
