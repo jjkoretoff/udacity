01b8efc0c8364cb36a66d3c9b1ef3a21eef2f547
jjkoretoff <jjkoretoff@gmail.com>
2016-04-27 12:50:48 -0400

    updates

--- intro-to-computer-science/cs1-problem-set.py
+++ intro-to-computer-science/cs1-problem-set.py
@@ -57,6 +57,71 @@ x1 = round(x, 0)
 print '{0:g}'.format(x1)
 
 
+# Replace the first occurrence of marker in the line below.
+# There will be at least one occurrence of the marker in the
+# line of text. Put the answer in the variable 'replaced'.
+# Hint: You can find out the length of a string by command
+# len(string). We might test your code with different markers!
+
+
+# Example 1
+marker = "AFK"
+replacement = "away from keyboard"
+line = "I will now go to sleep and be AFK until lunch time tomorrow."
+
+replaced = line.replace(marker, replacement)
+
+print replaced
+
+# Example 2 # uncomment this to test with different input
+marker = "EY"
+replacement = "Eyjafjallajokull"
+line = "The eruption of the volcano EY in 2010 disrupted air travel in Europe for 6 days."
+
+replaced = line.replace(marker, replacement)
+
+print replaced
+# Example 1 output should be:
+#>>> I will now go to sleep and be away from keyboard until lunch time tomorrow.
+# Example 2 output should be:
+#>>> The eruption of the volcano Eyjafjallajokull in 2010 disrupted air travel in Europe for 6 days.
+
+
+# A palindrome is a word or a phrase that reads 
+# the same backwards as forwards. Make a program 
+# that checks if a word is a palindrome. 
+# If the word is a palindrome, print 0 to the terminal,
+# -1 otherwise. 
+# The word contains lowercase letters a-z and 
+# will be at least one character long.
+#
+### HINT! ###
+# You can read a string backwards with the following syntax:
+# string[::-1] - where the "-1" means one step back.
+# This exercise can be solved with only unit 1 knowledge
+# (no loops or conditions)
+
+word = "madam"
+
+is_palindrome = word.find(word[::-1])
+
+# TESTING
+print is_palindrome
+
+# test case 2
+word = "madman" # uncomment this to test
+
+###
+# YOUR CODE HERE. DO NOT DELETE THIS LINE OR ADD A word DEFINITION BELOW IT.
+###
+
+is_palindrome = word.find(word[::-1])
+
+# TESTING
+print is_palindrome
+# >>> 0  # outcome if word == "madam"
+# >>> -1 # outcome if word == "madman"
+
 
 
 
